6b37840fcd986fb984c37f9563cc7d6d5dd840d5
koulache-yousra <yousraqllf17@gmail.com>
2025-11-20 17:08:52 +0100

    other updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,8 @@ const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: 'TeknoSys',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  description: 'Votre partenaire IT pour un environnement plus sécurisé ',
+  
   icons: {
     icon: [
       {
